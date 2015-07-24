44a5fa9f793e3ee86482b69ce5ed4970e716d4ca
William Jaggers <17790@cgs.act.edu.au>
2015-07-24 12:45:43 +1000

    Initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by William Jaggers on 24/07/2015.
+//  Copyright (c) 2015 William Jaggers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var gday = "hello"
+}
+
